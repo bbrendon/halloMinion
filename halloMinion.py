b05a96e37815a63d6a984e7df8df608ae1884329
risca <risca@eeepc>
2016-08-19 00:32:19 +0200

    code cleanup - to test

--- halloMinion.py
+++ halloMinion.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-import sys
+"""Return the lastseen time of every minion on the salt master.
+
+Usage:
+./haloMinion.py
+"""
 from os import listdir
-from os.path import isfile
 from os.path import isdir
 import datetime
 import os
@@ -9,24 +12,22 @@ import os.path
 
 
 def modification_date(filename):
-    t = os.path.getmtime(filename)
-    x = datetime.datetime.fromtimestamp(t)
-    return x
+    """Return the modification time of a file"""
+    time = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(time)
 
 def datetime2string(i):
-    r = i.strftime("%Y.%m.%d %H:%M:%S")
-    return r
+    """Return a nice string for a time"""
+    return i.strftime("%Y.%m.%d %H:%M:%S")
 
 ##########################################################################
 
-DIR_OF_DIR_pki = '/etc/salt/pki/master/minions/'
+DIRECTORY_PKI = '/etc/salt/pki/master/minions/'
 '''
-Only those Minons are known
+This directory stores all keys of known minions.
 '''
 
-
-
-DIR_OF_DIR_cache = '/var/cache/salt/master/minions/'
+DIRECTORY_CACHE = '/var/cache/salt/master/minions/'
 '''
 There are more directories than there are known minons
 In each directory there are two files.
@@ -34,12 +35,14 @@ In each directory there are two files.
 
 ##########################################################################
 
-all_known_minions = [ f for f in listdir(DIR_OF_DIR_pki) if isdir(DIR_OF_DIR_pki) ]
+KNOWN_MINIONS = [f for f in listdir(DIRECTORY_PKI) if isdir(DIRECTORY_PKI)]
 
-for one_minion in all_known_minions:
-    one_minion_cache_dir = os.path.join(DIR_OF_DIR_cache, one_minion)
+for one_minion in KNOWN_MINIONS:
+    one_minion_cache_dir = os.path.join(DIRECTORY_CACHE, one_minion)
     if isdir(one_minion_cache_dir):
-        age_of_d = modification_date(os.path.join(one_minion_cache_dir,'data.p'))
-        age_of_m = modification_date(os.path.join(one_minion_cache_dir,'mine.p'))
-        lastseen = max(age_of_d,age_of_m)
-        print datetime2string( lastseen)+ "\t" + one_minion
+        age_of_d = modification_date(os.path.join(one_minion_cache_dir,
+                                                  'data.p'))
+        age_of_m = modification_date(os.path.join(one_minion_cache_dir,
+                                                  'mine.p'))
+        lastseen = max(age_of_d, age_of_m)
+        print datetime2string(lastseen) + "\t" + one_minion
